51518d3987f62373e2df451085d024f55d0e58d9
Jinpeng Zhang <79638739+Jimpoz@users.noreply.github.com>
2024-03-28 15:05:24 +0100

    Added mongodb

--- TAW_Project/src/app/app.component.spec.ts
+++ TAW_Project/src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, TAW_Project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('TAW_Project app is running!');
   });
 });
