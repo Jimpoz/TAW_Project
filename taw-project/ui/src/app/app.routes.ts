9a74d4a93be39c873f6deba04fe1dcd7b1799349
Jinpeng Zhang <jimpo.jpz@gmail.com>
2024-05-19 23:50:49 +0200

    creationof login/signup components and first page of frontend

--- taw-project/ui/src/app/app.routes.ts
+++ taw-project/ui/src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule  } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { NgModel } from '@angular/forms';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '', redirectTo: 'signup', pathMatch: 'full'
+    },
+    {
+        path: 'login', component: LoginComponent
+    },
+    {
+        path: 'signup', component: SignupComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
+  
